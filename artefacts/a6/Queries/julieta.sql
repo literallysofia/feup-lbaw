8e5f40f54481d6b59aa3a14517c0af7781ca387d
julietafrade97 <up201506530@fe.up.pt>
2018-04-03 23:34:17 +0100

    frequency and name in queries

--- artefacts/a6/Queries/julieta.sql
+++ artefacts/a6/Queries/julieta.sql
@@ -1,60 +1,75 @@
-/*1 - GET ALL FAQS*/
+/*All faqs*/
+/*units per day*/
 SELECT question, answer FROM faqs;
 
-/*2 - SIGN IN*/
+/*Sign in*/
+/*hundreds per day*/
 SELECT id
 FROM users
 WHERE username = $username
 AND password = $hashedPassword;
 
+/*Sign in 2*/
+/*hundreds per day*/
 SELECT id
 FROM users
 WHERE email = $email
 AND password = $hashedPassword;
 
-/*3 - SIGN UP*/
+/*Sign up*/
+/*units per day*/
 INSERT INTO users (name, username, email, password)
 VALUES ($name, $username, $email, $hashedPassword);
 
-/*4 - USER INFO FOR PROFILE*/
+/*User Info*/
+/*thousands per day*/
 SELECT name, username, email
 FROM users
 WHERE id = $id;
 
+/*Update User Info*/
+/*units per day*/
 UPDATE users
 SET name=$name, username=$username, email=$email, password=$hashedPassword
 WHERE id=$id;
 
-/*5 - GET USER ADRESSES*/
+/*NOT TESTED*/
+/*User Addresses*/
+/*dozens per day*/
 SELECT A.id, A.name, A.street, A."postal_code", CTY.name, CNTR.name
 FROM addresses AS A, cities AS CTY, countries AS CNTR
 WHERE A."user_id" = $id
 AND A."city_id" = CTY.id
 AND CTY."country_id" = CNTR.id
-AND A.archived NOT(true);
+AND A.isArchived = false;
 
+/*NOT TESTED*/
+/*Update Archive Address*/
+/*units per day*/
 UPDATE addresses
-SET archived=$archived
-WHERE id=$id
+SET isArchived=$isArchived
+WHERE id=$id;
 
-/*-------------------------------------------------------------NOT TESTED-------------------------------------------------------------*/
-/*6 - GET USER PURCHASES OF A CERTAIN TYPE*/
+/*User Purchases of a Certain Type*/
+/*dozens per day*/
 SELECT PRCHS.id, PRCHS."date", PRCHS.status, PRCHS.total
-FROM users AS U, purchases AS PRCHS
-WHERE PRCHS."user_id" = $id,
-AND PRCHS.status = $type
+FROM purchases AS PRCHS
+WHERE PRCHS."user_id" = $id
+AND PRCHS.status = $type;
 
-/*8 - GET PURCHASE PRODUCTS*/
+/*Products of a purchase*/
+/*dozens per day*/
 SELECT PRDCT.id, PRDCT.name, PP.price, PP.quantity
-FROM purchases AS PRCHS, products AS PRDCT, product_purchases AS PP,
-WHERE PRCHS.id = $id,
-AND PP."purchase_id" = PRCHS.id,
+FROM purchases AS PRCHS, products AS PRDCT, product_purchases AS PP
+WHERE PRCHS.id = $id
+AND PP."purchase_id" = PRCHS.id
 AND PP."product_id" = PRDCT.id;
 
-/*9 - GET PURHASE ADDRESS*/
+/*Address of a Purchase*/
+/*dozens per day*/
 SELECT A.street, A."postal_code", CTY.name, CNTR.name
 FROM purchases AS P, addresses AS A, cities AS CTY, countries AS CNTR
-WHERE P.id = $id,
-AND P."address_id" = A.id,
-AND A."city_id" = CTY.id,
+WHERE P.id = $id
+AND P."address_id" = A.id
+AND A."city_id" = CTY.id
 AND CTY."country_id" = CNTR.id;
