df67c1bace2650c382bf0716fa3e5ef098cbad89
Luís Martins <luis.n.martins97@gmail.com>
2018-04-04 11:00:05 +0100

    Update triggers.sql

--- artefacts/a6/triggers.sql
+++ artefacts/a6/triggers.sql
@@ -19,6 +19,24 @@ EXECUTE PROCEDURE update_product_score();
 
 /*Quando adiciona uma compra a quantidade de cada produto não pode ser maior que a quantidade disponível do mesmo*/
 
+CREATE FUNCTION check_purchases_quantities() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+	IF 
+		NOT EXISTS (SELECT quantity_available FROM products WHERE id = New.product_id AND quantity_available >= New.quantity) 
+	THEN
+		RAISE EXCEPTION ’You can’t buy % items of product %’ , New.quantity, New.product_id
+	END IF;
+	RETURN NEW;
+END
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER check_purchase_quantities BEFORE INSERT
+ON product_purchases
+FOR EACH ROW
+EXECUTE PROCEDURE check_purchases_quantities();
+
 
 /*Quando é adicionada uma compra limpa o cart do respetivo user*/
 CREATE FUNCTION clear_cart() RETURNS TRIGGER AS
@@ -67,22 +85,45 @@ EXECUTE PROCEDURE update_available_products();
 
 /*Quando uma morada for arquivada e nao existirem compras associadas esta deve ser apagada*/
 
+/*Quando uma categoria não tem produtos esta nao pode estar na navbar*/
 
-/*Quando um produto é arquivado se não tiver compras associadas deve ser apagado*/
-CREATE FUNCTION delete_product() RETURNS TRIGGER AS
+	/*quando coloca uma categoria como estando na navbar esta tem que ter produtos*/
+
+	CREATE FUNCTION navbar_category() RETURN TRIGGER AS
+	$BODY$OD
+	BEGIN
+		IF (New.is_navbar_category = TRUE) THEN
+			IF NOT EXISTS (SELECT P.id FROM products AS P WHERE P.category_id = New.id) 
+		THEN
+			RAISE EXCEPTION ’A navbar category need to have products’
+		END IF;
+		RETURN NEW;
+	END
+	$BODY$
+	LANGUAGE plpgsql;
+
+	CREATE TRIGGER navbar_category BEFORE UPDATE
+	ON categories
+	FOR EACH ROW
+	EXECUTE PROCEDURE navbar_category();
+
+/*Quando um produto é arquivado este tem que ser removido de todas as wishlists e carts*/
+
+
+CREATE FUNCTION archive_product() RETURN TRIGGER AS
 $BODY$
 BEGIN
-  DELETE FROM products
-  WHERE "product_id" = New."product_id"
-  AND "product_id" NOT IN (SELECT "product_id" FROM product_purchases)
+	DELETE FROM whishlists
+	WHERE product_id = NEW.id
+	DELETE FROM product_carts
+	WHERE product_id = NEW.id
+	
 END
 $BODY$
 LANGUAGE plpgsql;
 
-CREATE TRIGGER delete_product AFTER INSERT
+CREATE TRIGGER archive_product AFTER INSERT
 ON archived_products
-EXECUTE PROCEDURE delete_product();
+FOR EACH ROW
+EXECUTE PROCEDURE archive_product();
 
-/*Quando uma categoria não tem produtos esta nao pode estar na navbar*/
-
-/*Quando um produto é arquivado este tem que ser removido de todas as wishlists e carts*/
