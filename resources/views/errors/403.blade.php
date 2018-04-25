adf77014d592cfc0a5cf42a2a93a45828d26cec5
Luís Martins <luis.n.martins97@gmail.com>
2018-04-25 11:33:00 +0100

    error pages updated

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title','Access Forbidden')
+
+@section('content')
+
+<main>
+<div class="container">
+    <h1>ACCESS FORBIDDEN</h1>
+</div>
+</main>
+@endsection
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,3 +1,12 @@
-<?php
+@extends('layouts.app')
 
-?>
+@section('title','Not Found')
+
+@section('content')
+
+<main>
+<div class="container">
+    <h1>NOT FOUND</h1>
+</div>
+</main>
+@endsection
